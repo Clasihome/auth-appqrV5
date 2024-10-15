38a712c38544f34f2c415a6646f1c21fb73f6a6b
“joellopez”775 <joelignaciolopezbastias@gmail.com>
2024-10-15 19:07:20 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'auth-appqrv5',
+  webDir: 'www'
+};
+
+export default config;
